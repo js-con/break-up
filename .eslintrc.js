13245f24b71a91432b6936d002c7d2ce23f6dbee
lbw <574524020@qq.com>
2022-05-19 17:09:18 +0800

    chore:eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,43 @@
 module.exports = {
-  extends: [
-    'alloy',
-    'alloy/react',
-  ],
-  env: {
-    // 你的环境变量（包含多个预定义的全局变量）
-    //
-    // browser: true,
-    // node: true,
-    // mocha: true,
-    // jest: true,
-    // jquery: true
-  },
-  globals: {
-    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
-    //
-    // myGlobal: false
-  },
-  rules: {
-    // 自定义你的规则
-  },
-};
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'alloy',
+		'alloy/react'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
