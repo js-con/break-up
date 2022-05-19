4bb18d2381a0cd9c877a2c3aa021fe39574942a3
lbw <574524020@qq.com>
2022-05-19 18:19:50 +0800

    chore:eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    '@antfu/eslint-config',
+    '@antfu/eslint-config-react',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'never',
-    ],
-  },
 }
