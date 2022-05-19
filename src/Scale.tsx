c506c1a6ed6fcb97bcefbeb2630e09abe93b29b1
John Walker <ss.lbw.ss@gmail.com>
2022-05-19 22:15:55 +0800

    feat: radio unfinished

--- src/Scale.tsx
+++ src/Scale.tsx
@@ -1,5 +1,12 @@
-import { Button, ButtonGroup, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 import * as React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  width: 84px;
+  height: 32px;
+  color: black;
+  border-radius: 8px;
+`
 
 export interface Question extends Record<string, unknown> {
   title: string
@@ -10,57 +17,65 @@ export interface ScaleProps {
   scale: Question[]
 }
 
+const Radio: React.FC<{
+  checked: boolean
+}> = (props) => {
+  const checked = React.useState(props.checked)
+  return <input type="radio" checked={checked} onChange={() => handleChange} />
+}
+
+const Form: React.FC<{ question: Question }> = ({ question }) => {
+  const { title, options } = { ...question }
+  const list = options.map((option, index) => {
+    return {
+      text: option,
+      key: index,
+      checked: false,
+    }
+  })
+  const onChange = (item: any) => {
+    item.checked = true
+  }
+  return (
+    <div className="card">
+      <div className="card-title">{title}</div>
+      {list.map((item) => (
+        <div key={item.key}>
+          <div>{item.text}</div>
+          <Radio></Radio>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export default function Scale(props: ScaleProps) {
-  const { scale } = props
+  const { scale } = { ...props }
   const [page, setPage] = React.useState(0)
   const [question, setQuestion] = React.useState(scale[page])
   function onNext() {
     if (page < scale.length - 1) {
-      console.log(1)
       setPage(page + 1)
     }
   }
   function onPrev() {
     if (page >= 0) {
-      console.log(1)
       setPage(page - 1)
     }
   }
   React.useEffect(() => {
     if (scale[page]) {
-      console.log('todo')
       setQuestion(scale[page])
     }
   }, [page])
 
   return (
     <div>
-      <Form title={question.title} options={question.options} />
+      <Form question={question} />
       <footer>
-        <ButtonGroup color="secondary" aria-label="medium secondary button group">
-          <Button onClick={() => onPrev()}>上一题</Button>
-          <Button onClick={() => onNext()}>下一题</Button>
-        </ButtonGroup>
+        <Button onClick={() => onPrev()}>上一题</Button>
+        <Button onClick={() => onNext()}>下一题</Button>
       </footer>
-    </div>
-  )
-}
-
-function Form(props: Question) {
-  const { title, options } = props
-  return (
-    <div className="card">
-      <FormLabel className="card-title">{title}</FormLabel>
-      <RadioGroup className="card-options">
-        {options.map((option, key) => (
-          <FormControlLabel
-            value={key}
-            control={<Radio />}
-            label={option}
-            key={key}
-          ></FormControlLabel>
-        ))}
-      </RadioGroup>
     </div>
   )
 }
