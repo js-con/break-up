3c80c7e1f2dab6c09d250dd4bc396cac4a007963
lbw <574524020@qq.com>
2022-05-20 14:42:18 +0800

    refactor:refactor

--- src/Scale.tsx
+++ src/Scale.tsx
@@ -13,20 +13,24 @@ export interface Question extends Record<string, unknown> {
   options: string[]
 }
 
-export interface ScaleProps {
-  scale: Question[]
+interface FormItem {
+  title: string
+  options: {
+    text: string
+    checked: boolean
+  }[]
+  key: number
 }
 
-const Form: React.FC<{ question: Question }> = ({ question }) => {
+const Form: React.FC<{ question: FormItem }> = ({ question }) => {
   const [title, setTitle] = React.useState('')
-  const [list, setList] = React.useState<any[]>([])
+  const [options, setOptions] = React.useState<FormItem['options']>([])
   React.useEffect(() => {
-    const { options, title } = { ...question }
-    setList([...options.map((option, index) => ({ text: option, key: index, checked: false }))])
-    setTitle(title)
+    setOptions(question.options)
+    setTitle(question.title)
   }, [question])
   const handleChange = (index: number) => {
-    const newList = list.map((item, i) => {
+    const newOptions = options.map((item, i) => {
       if (i === index) {
         item.checked = true
       } else {
@@ -35,38 +39,52 @@ const Form: React.FC<{ question: Question }> = ({ question }) => {
       return item
     })
 
-    setList([...newList])
+    setOptions([...newOptions])
   }
   return (
     <div className="card">
       <div className="card-title">{title}</div>
-      {list.map((item) => (
-        <div key={item.key}>
-          <div>{item.text}</div>
-          <input type="radio" checked={item.checked} onChange={() => handleChange(item.key)} />
+      {options.map((item, index) => (
+        <div key={index}>
+          <span>{item.text}</span>
+          <input type="radio" checked={item.checked} onChange={() => handleChange(index)} />
         </div>
       ))}
     </div>
   )
 }
 
-export default function Scale(props: ScaleProps) {
-  const { scale } = { ...props }
+function initScale(scale: Question[]) {
+  const list = scale.map((item, index) => {
+    return {
+      title: item.title,
+      options: item.options.map((i) => ({ text: i, checked: false })),
+      key: index,
+    }
+  })
+  return list
+}
+export default function Scale(props: { scale: Question[] }) {
+  const [forms, setForms] = React.useState<FormItem[]>(initScale(props.scale))
   const [page, setPage] = React.useState(0)
-  const [question, setQuestion] = React.useState(scale[page])
+  const [question, setQuestion] = React.useState(forms[page])
+  React.useEffect(() => {
+    const { scale } = { ...props }
+    setForms(initScale(scale))
+  }, [props])
   function onNext() {
-    if (page < scale.length - 1) {
+    if (page < forms.length - 1) {
       setPage(page + 1)
     }
   }
   function onPrev() {
-    if (page >= 0) {
+    if (page > 0) {
       setPage(page - 1)
     }
   }
   React.useEffect(() => {
-    if (scale[page]) {
-      setQuestion(scale[page])
+    if (forms[page]) {
+      setQuestion(forms[page])
     }
   }, [page])
 
