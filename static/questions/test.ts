65838a1bf26e659cc09cb96e0bdd2a1d9e33da02
lbw <574524020@qq.com>
2022-05-23 18:56:36 +0800

    feat:test questions

---
+++ static/questions/test.ts
@@ -0,0 +1,144 @@
+export const socialMedia = [
+  {
+    title: 'What\'s your opinion on the new Facebook?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Twitter?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Instagram?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Snapchat?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Pinterest?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Tumblr?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Reddit?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new LinkedIn?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+  {
+    title: 'What\'s your opinion on the new Tumblr?',
+    options: [
+      'I\'m not sure',
+      'I don\'t like it',
+      'I like it',
+      'I don\'t care',
+    ],
+  },
+]
+
+export const diet = [
+  {
+    title: 'what is your favorate food?',
+    options: [
+      'banana',
+      'apple',
+      'melon',
+      'peach',
+    ],
+  },
+  {
+    title: 'what is your favorate drink?',
+    options: [
+      'coke',
+      'pepsi',
+      'water',
+      'milk',
+    ],
+  },
+  {
+    title: 'what is your favorate dessert?',
+    options: [
+      'cake',
+      'ice cream',
+      'pie',
+      'pudding',
+    ],
+  },
+  {
+    title: 'what is your favorate snack?',
+    options: [
+      'chips',
+      'pizza',
+      'sandwich',
+      'burger',
+    ],
+  },
+  {
+    title: 'what is your favorate meat?',
+    options: [
+      'chicken',
+      'pork',
+      'beef',
+      'lamb',
+    ],
+  },
+]
+
+export default [
+  {
+    name: 'socialMedia',
+    introduction: 'your opinion on social media',
+    questions: socialMedia,
+  },
+  {
+    name: 'diet',
+    introduction: 'your favorate diet',
+    questions: diet,
+  },
+]
