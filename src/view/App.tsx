8fe930312ef590c5ea8d824b5e7f8fa86bbbd31b
lbw <574524020@qq.com>
2022-05-23 19:14:39 +0800

    feat:basic routes

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,10 +1,32 @@
-import * as React from 'react'
-
+import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import questions from 'static/questions/test'
+import Scale from './Scale'
 function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/scale:question" element={<Scale />}></Route>
+      </Routes>
+  )
+}
+
+function Home() {
   return (
     <div className=" h-screen w-screen flex justify-center items-center bg-dark-300 text-white">
       <div className="text-center">
-        <h1>欢迎使用break up!</h1>
+        <h1 className="text-[2.23rem]">欢迎使用break up!</h1>
+        <h2 className="my-[20px] text-[1.5rem]">请选择问题集</h2>
+        <ul>
+          {questions.map((q, i) => (
+            <li key={i} className="mt-[12px] p-[12px] bg-gray-400 text-black">
+              <div>
+                <p className="text-[1.5rem]">{q.name}</p>
+                <p className="mt-[4px]">{q.introduction}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
