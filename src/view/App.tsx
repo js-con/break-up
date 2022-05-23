c72495a2019142185e4143bd8cd7cb1da81b747d
lbw <574524020@qq.com>
2022-05-23 19:27:13 +0800

    route:Sclae

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/scale:question" element={<Scale />}></Route>
+        <Route path="/scale" element={<Scale />}></Route>
       </Routes>
   )
 }
