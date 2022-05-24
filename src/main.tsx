f1bbc25cd557ab6bae67bad20879fd8248c71ad5
lbw <574524020@qq.com>
2022-05-24 16:58:41 +0800

    feat:provide theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import App from './view/App'
 import './style/index.css'
 import 'virtual:windi.css'
+import theme from './style/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>,
+    </ThemeProvider>,
 )
---
+++ src/style/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+export default createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+      light: '#434b79',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+})
