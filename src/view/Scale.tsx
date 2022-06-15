89aa13f2ce89a4928659bcdbb9c815df2bef4084
lbw <574524020@qq.com>
2022-06-15 18:57:24 +0800

    refactor:  Scale

--- src/view/Scale.tsx
+++ src/view/Scale.tsx
@@ -1,136 +1,58 @@
 import * as React from 'react'
 import { useLocation } from 'react-router-dom'
+import { Paper } from '@mui/material'
 import Button from '@mui/material/Button'
-import { Alert, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, Snackbar } from '@mui/material'
 import { useToasts } from '../components/Toast'
-import { AESencrypt } from '../shared/utils'
+import type { ScaleForm } from '../components/Scales/types'
+import Nominal from '../components/Scales/NominalScale'
+import Ordinal from '../components/Scales/OrdinalScale'
 
-export interface Question extends Record<string, unknown> {
-  title: string
-  options: string[]
-}
-
-interface FormItem {
-  title: string
-  options: {
-    text: string
-    checked: boolean
-  }[]
-  key: number
-}
-
-const Form: React.FC<{ question: FormItem; handleChange: (i: number) => void }> = ({ question, handleChange }) => {
-  const [title, setTitle] = React.useState('')
-  const [options, setOptions] = React.useState<FormItem['options']>([])
-
-  React.useEffect(() => {
-    setOptions(question.options)
-    setTitle(question.title)
-  }, [question])
-
-  return (
-    <FormControl>
-      <div className="mb-[8px] p-[8px] text-[1.5rem] leading-normal">{title}</div>
-      <RadioGroup>
-        {options.map((item, index) => (
-          <FormControlLabel
-            key={index}
-            control={
-              <Radio checked={item.checked} onChange={() => handleChange(index)} />
-            }
-            label={item.text}
-          />
-        ))}
-      </RadioGroup>
-    </FormControl>
-  )
-}
-
-function initScale(questions: Question[]) {
-  const scale = questions.map((item, index) => {
-    return {
-      title: item.title,
-      options: item.options.map(i => ({ text: i, checked: false })),
-      key: index,
-    }
-  })
-  return scale
-}
-
-function initAnswer(scale: ReturnType<typeof initScale>) {
-  const Answer = scale.map((question) => {
-    return question.options.findIndex((option) => {
-      return option.checked === true
-    })
-  })
-
-  return Answer
-}
-
-export default function Scale() {
+const Scale: React.FC = () => {
   const location = useLocation()
-  const { question } = location.state as any
-  const [forms, setForms] = React.useState(initScale(question))
-  const [page, setPage] = React.useState(0)
+  const { form } = location.state as { form: ScaleForm }
+
+  const [curPage, setCurPage] = React.useState<any>()
+  const [pageNo, setPageNo] = React.useState(0)
   const toast = useToasts()
 
   React.useEffect(() => {
-    const { question } = location.state as any
-    setForms(initScale(question))
-  }, [location])
+    setCurPage(form.content[pageNo])
+  }, [form, pageNo])
+
+  function Content() {
+    switch (form.type) {
+      case 'nominal':
+        return <Nominal content={curPage}/>
+      case 'ordinal':
+        return <Ordinal content={curPage}/>
+    }
+  }
 
   function onPrev() {
-    if (page > 0)
-      setPage(page - 1)
-  }
-
-  const handleChange = (index: number) => {
-    const newForms = forms.map((item, idx) => {
-      if (idx === page) {
-        item.options.forEach((option, key) => {
-          option.checked = key === index
-        })
-      }
-      return item
-    })
-    setForms(newForms)
-  }
-  function validate() {
-    if (forms[page].options.every(item => !item.checked)) {
-      toast.error('请选择回答')
-      return false
-    }
-    return true
+    setPageNo(pageNo - 1)
   }
 
   function onNext() {
-    if (!validate())
-      return
-
-    if (page < forms.length - 1)
-      setPage(page + 1)
+    setPageNo(pageNo + 1)
   }
 
   function onSubmit() {
-    if (!validate())
-      return
-    const answer = AESencrypt(initAnswer(forms).join(''))  
-    console.log(answer)
+    toast.success('success')
   }
 
   return (
     <>
       <Paper className="p-[16px] w-[80%] flex flex-col justify-center items-center">
-        <Form question={forms[page]} handleChange={handleChange}/>
+         <Content />
         <footer className="mt-[20px] w-[80%] flex justify-around">
           <Button
             onClick={() => onPrev()}
-            disabled={page <= 0}
+            disabled={pageNo <= 0}
             variant="contained"
           >
             上一题
           </Button>
-          {page < forms.length - 1
+          {pageNo < form.content.length - 1
             ? (
             <Button onClick={() => onNext()} variant="contained">下一题</Button>
               )
@@ -140,6 +62,7 @@ export default function Scale() {
         </footer>
       </Paper>
     </>
-
   )
 }
+
+export default Scale
