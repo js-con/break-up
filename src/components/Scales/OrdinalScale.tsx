e2ccae7f8571d3a8879b6050db8c76de77be0e70
lbw <574524020@qq.com>
2022-06-16 11:08:52 +0800

    feat:  ORdinalScale

--- src/components/Scales/OrdinalScale.tsx
+++ src/components/Scales/OrdinalScale.tsx
@@ -1,12 +1,61 @@
 import * as React from 'react'
+import { FormControl, Slider } from '@mui/material'
 import type { OrdinalScale } from './types'
 
 type ScaleContentMember = OrdinalScale['content'][number]
-const Ordinal: React.FC<{ content: ScaleContentMember }> = ({ content }) => {
+type ScaleConfig = OrdinalScale['config']
+interface Mark {
+  value: number
+  label?: string
+}
+const Ordinal: React.FC<{
+  content: ScaleContentMember
+  config: ScaleConfig
+  checked: number
+  handleChange: Function
+}> = ({ content, config, checked, handleChange }) => {
+  const [title, setTitle] = React.useState('')
+  const [marks, setMarks] = React.useState<Mark[]>([])
+  const [value, setValue] = React.useState(0)
+
+  React.useLayoutEffect(() => {
+    setTitle(content.title)
+    setValue(checked ?? config.defaultDegree - 1)
+    setMarks(createMarks(config))
+  }, [content, config])
+
+  function createMarks(config: ScaleConfig) {
+    return new Array(config.degree).fill({}).map((_, idx) => {
+      if (idx === 0) {
+        return {
+          value: idx,
+          label: config.leftText,
+        }
+      }
+      if (idx === config.degree - 1) {
+        return {
+          value: idx,
+          label: config.rightText,
+        }
+      }
+      return {
+        value: idx,
+      }
+    })
+  }
+
   return (
-    <div>
-      {JSON.stringify(content)}
-    </div>
+      <FormControl>
+        <div className="mb-[8px] p-[8px] text-[1.5rem] leading-normal">{title}</div>
+        <Slider
+          value={value}
+          step={1}
+          marks={marks}
+          min={0}
+          max={marks.length - 1}
+          onChange={(_, val) => handleChange(val)}
+        />
+      </FormControl>
   )
 }
 
