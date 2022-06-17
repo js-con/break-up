d8f90ec4175193b24b02b95f334ee379ff7e28a9
lbw <574524020@qq.com>
2022-06-17 17:04:28 +0800

    refactor:  static dir

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,6 +1,6 @@
 import { Card, List, ListItem, ListItemText } from '@mui/material'
 import { Link, Route, Routes } from 'react-router-dom'
-import questions from 'static/questions/test'
+import scales from 'static/scales'
 import Scale from './Scale'
 
 function App() {
@@ -20,17 +20,17 @@ function Home() {
         <h1 className="text-[2.23rem]">欢迎使用break up!</h1>
         <h2 className="my-[20px] text-[1.5rem]">请选择问题集</h2>
         <List>
-          {questions.map((q, i) => (
+          {scales.map((s, i) => (
             <ListItem key={i}>
               <Card className="w-[100%]">
                 <Link
                   to="/scale"
-                  state={{ form: q.questions }}
+                  state={{ form: s.body }}
                   className="block p-[12px] no-underline text-black"
                 >
                   <ListItemText
-                    primary={q.name}
-                    secondary={q.introduction}
+                    primary={s.name}
+                    secondary={s.introduction}
                     className="text-white"
                   />
                 </Link>
---
+++ static/scales/index.ts
@@ -0,0 +1,9 @@
+import { breakUp } from './breakUp'
+
+export default [
+  {
+    name: 'break-up',
+    introduction: 'your opinion on world, society and humanity',
+    body: breakUp,
+  },
+]
